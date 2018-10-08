77ca3f5eb6fa43a475dee76c3ae115b23c8acee4
LeeVaughn <dlvprogramming@gmail.com>
2018-10-07 17:13:29 -0700

    added routes for photos, gifs and quotes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+// GET home route
 router.get("/", function(req, res, next) {
   res.render("index", { title: "The Happiness Primer" });
+});
+
+// GET photos route
+router.get("/photos", function(req, res, next) {
+  res.render("photos");
+});
+
+// GET gifs route
+router.get("/gifs", function(req, res, next) {
+  res.render("gifs");
+});
+
+// GET quotes route
+router.get("/quotes", function(req, res, next) {
+  res.render("quotes");
 });
 
 module.exports = router;
