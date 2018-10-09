1e0988d80da7941b981936684572f51d35d00ae1
LeeVaughn <dlvprogramming@gmail.com>
2018-10-09 16:06:30 -0700

    processed data from GIPHY API and included it in the template

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
+// const flickrAPI = require("../.flickrConfig.js");
+const giphyAPI = require("../.giphyConfig.js");
+const limit = 12;
+let gifs = [];
+let photos = [];
 
 // GIPHY API call
-const getGifs = axios.get("http://api.giphy.com/v1/gifs/trending?api_key=RfkIjBEloXXrKNYJIjqijXyuXJJYFDUx")
-  .then(function (response) {
-    console.log(response);
+axios.get(`http://api.giphy.com/v1/gifs/search?q=cute+animals&api_key=${giphyAPI}&limit=${12}`)
+  .then(function (res) {
+    console.log("GIPHY API call successful");
+    for (let i = 0; i < limit; i++) {
+      const gif = {
+        "url": res.data.data[i].images.downsized.url
+      }
+      gifs.push(gif);
+    }
   })
-  .catch(function (error) {
-    console.log(error);
+  .catch(function (err) {
+    console.log("Error making GIPHY call");
   });
 
 // GET home route
@@ -23,7 +34,7 @@ router.get("/photos", function(req, res, next) {
 
 // GET gifs route
 router.get("/gifs", function(req, res, next) {
-  res.render("gifs");
+  res.render("gifs", { gifs });
 });
 
 // GET quotes route
