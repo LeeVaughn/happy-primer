fe9e26b33d59dd887f20ba9597f9c5f7d44c613a
LeeVaughn <dlvprogramming@gmail.com>
2018-10-09 20:03:26 -0700

    required body-parser and mongoose as dependencies and set up db connection

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
@@ -18,6 +19,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// connects to mongoDB
+mongoose.connect("mongodb://localhost:27017/quotes", { useNewUrlParser: true });
+const db = mongoose.connection;
+
+// mongo error
+db.on("error", console.error.bind(console, "connection error:"));
+
+// listens for open event
+db.once("open", () => {
+  console.log("Successfully connected to the database");
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
