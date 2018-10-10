c6a54299759df9be23420b76e59f9293c10e4c68
LeeVaughn <dlvprogramming@gmail.com>
2018-10-10 13:12:11 -0700

    converted some functions to arrow functions, disabled quotes post route for production

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api
 
     // loops over response data to create an array of urls for the individual photos
     for (let i = 0; i < limit; i++) {
-      // variables used to construct url string
+      // variables used to construct url string for each photo
       const farm = res.data.photos.photo[i].farm;
       const server = res.data.photos.photo[i].server;
       const id = res.data.photos.photo[i].id;
@@ -49,28 +49,28 @@ axios.get(`http://api.giphy.com/v1/gifs/search?q=cute+animals&api_key=${giphyAPI
   });
 
 // GET home route
-router.get("/", function(req, res, next) {
+router.get("/", (req, res, next) => {
   res.render("index", { title: "The Happiness Primer" });
 });
 
 // GET photos route
-router.get("/photos", function(req, res, next) {
+router.get("/photos", (req, res, next) => {
   res.render("photos", { photos });
 });
 
 // GET gifs route
-router.get("/gifs", function(req, res, next) {
+router.get("/gifs", (req, res, next) => {
   res.render("gifs", { gifs });
 });
 
 // GET quotes route
-router.get("/quotes", function(req, res, next) {
+router.get("/quotes", (req, res, next) => {
   // creates random number based on number of documents in the Quotes database
   const index = Math.floor(Math.random() * 20);
 
    // returns all quotes
   Quote.find()
-    .exec(function (err, quote) {
+    .exec( (err, quote) => {
       if (err) {
         next(err);
       } else {
@@ -83,18 +83,18 @@ router.get("/quotes", function(req, res, next) {
     });
 });
 
-// POST quotes route
-router.post("/quotes", (req, res, next) => {
-  const quote = new Quote(req.body);
+// // POST quotes route (disabled for production)
+// router.post("/quotes", (req, res, next) => {
+//   const quote = new Quote(req.body);
 
-  quote.save((err, quote) => {
-    if (err) {
-      next(err);
-    }
-    res.status(201);
-    res.location("/");
-    res.send();
-  });
-});
+//   quote.save((err, quote) => {
+//     if (err) {
+//       next(err);
+//     }
+//     res.status(201);
+//     res.location("/");
+//     res.send();
+//   });
+// });
 
 module.exports = router;
