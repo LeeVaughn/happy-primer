b6e434c5782e30e5c69caf034c8f12e3610f235c
LeeVaughn <dlvprogramming@gmail.com>
2018-11-28 22:29:15 -0800

    removed old API requests

--- routes/index.js
+++ routes/index.js
@@ -9,28 +9,6 @@ let photos = [];
 const Quote = require("../models/quote");
 
 // Flickr API call
-// axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrAPI}&tags=dogs+surfing&in_gallery=&per_page=${limit}&format=json&nojsoncallback=1`)
-//   .then(function (res) {
-//     console.log("Flickr API call successful");
-
-//     // loops over response data to create an array of urls for the individual photos
-//     for (let i = 0; i < limit; i++) {
-//       // variables used to construct url string for each photo
-//       const farm = res.data.photos.photo[i].farm;
-//       const server = res.data.photos.photo[i].server;
-//       const id = res.data.photos.photo[i].id;
-//       const secret = res.data.photos.photo[i].secret;
-
-//       const photo = {
-//         "url": `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
-//       }
-//       photos.push(photo);
-//     }
-//   })
-//   .catch(function (err) {
-//     console.log("Error with Flickr API");
-//   });
-
 flickrCall = (query = "dogs surfing") => {
   axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrAPI}&tags=${query}&in_gallery=&per_page=${limit}&format=json&nojsoncallback=1`)
   .then(function (res) {
@@ -56,22 +34,6 @@ flickrCall = (query = "dogs surfing") => {
 }
 
 // GIPHY API call
-// axios.get(`http://api.giphy.com/v1/gifs/search?q=cute+animals&api_key=${giphyAPI}&limit=${limit}`)
-//   .then(function (res) {
-//     console.log("GIPHY API call successful");
-
-//     // loops over response data to create an array of urls for the individual photos
-//     for (let i = 0; i < limit; i++) {
-//       const gif = {
-//         "url": res.data.data[i].images.downsized.url
-//       }
-//       gifs.push(gif);
-//     }
-//   })
-//   .catch(function (err) {
-//     console.log("Error with GIPHY API");
-//   });
-
 giphyCall = (query = "cute animals") => {
   axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${giphyAPI}&limit=${limit}`)
   .then(function (res) {
